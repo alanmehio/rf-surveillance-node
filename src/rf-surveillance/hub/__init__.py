72cebec064ea554ca29703d5c85cdc23622edca1
alanmehio <alan.mehio@gmail.com>
2025-02-17 22:29:47 +0100

    more commit to build the project structure

---
+++ src/rf-surveillance/hub/__init__.py
@@ -0,0 +1,10 @@
+"""
+
+| **RF Node**, a *Python Package* Alan explain the node concept here .... 
+
+| Goal of the project is to build a ... node and hub etc...  
+  
+"""
+# The below version not used in build from tox
+# build version is found in pyproject.toml 
+__version__ = '0.1.0'

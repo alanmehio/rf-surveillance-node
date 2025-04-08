60bad5866a50fe4c46e2ab07379bbb50ccb3da81
Alan <alan.mehio@gmail.com>
2025-04-08 22:12:04 +0200

    adding more file

---
+++ src/rfclient/main.py
@@ -0,0 +1,13 @@
+
+'''
+$ export PYTHONPATH=/home/alan/workspace-python/RTL-SDR/rf-surveillance-center/src
+$ pwd 
+ /home/alan/workspace-python/RTL-SDR/rf-surveillance-center/src
+$ python3 rfclient ????
+'''
+def main()-> None:
+   print("main..")
+
+# this is important so that it does not run from pytest 
+if __name__ == "__main__": 
+    main()
---
+++ src/rfserver/__init__.py
@@ -0,0 +1,14 @@
+"""
+
+| **RF Node**, a *Python Package* Alan explain the node concept here .... 
+
+| Goal of the project is to build a ... node and hub etc...  
+  
+"""
+
+from version import version as __version__
+
+__all__ = (
+    "__version__",
+    "main",
+)

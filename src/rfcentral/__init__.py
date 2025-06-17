b4ce439fff3b2cbbb20b9137e3373014c08c5b71
alanmehio <alan.mehio@gmail.com>
2025-06-17 22:50:52 +0200

    adding preliminary rest service

--- src/rfcentral/__init__.py
+++ src/rfcentral/__init__.py
@@ -1,9 +1,9 @@
 """
 
-| **RF Central**, a *Python Package* receive high power RF data from different nodes 
+| **RF Central**, a *Python Package* receive high power RF data from different nodes
 
-| 
-  
+|
+
 """
 
 __version__ = "0.0.1"
@@ -12,3 +12,6 @@ __all__ = (
     "__version__",
     "main",
 )
+
+# bootstrap it
+from .rest_api import rest
